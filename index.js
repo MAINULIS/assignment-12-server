249b0a364ae8e3b0e18d26f3f70a147e8c0ff52d
Mainul Islam <mainulislam6168@gmail.com>
2024-05-19 22:59:12 +0600

    selected course delete by id api created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -87,6 +87,12 @@ async function run() {
             }
             const query = {email: email};
             const result = await selectedCollection.find(query).toArray();
+            res.send(result);
+        })
+        app.delete('/selected/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id)};
+            const result = await selectedCollection.deleteOne(query);
             res.send(result);
         })
         
